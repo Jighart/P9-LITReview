406dec7c4aa4207690347977d40a223c36649220
Ludovic Andrianoff <landrianoff2@gmail.com>
2023-01-18 21:34:30 +0100

    Updated model, finished admin setup

--- LITReview/blog/admin.py
+++ LITReview/blog/admin.py
@@ -1,7 +1,12 @@
 from django.contrib import admin
 from .models import Ticket, Review, UserFollow
 
-# Register your models here.
-admin.site.register(Ticket)
+@admin.register(Ticket)
+class TicketAdmin(admin.ModelAdmin):
+    list_display = ('title', 'user', 'time_created')
+    search_fields = ('title', 'body')
+    ordering = ('time_created', 'user')
+    list_filter = ('time_created',)
+
 admin.site.register(Review)
 admin.site.register(UserFollow)
--- LITReview/blog/models.py
+++ LITReview/blog/models.py
@@ -7,7 +7,7 @@ from django.utils import timezone
 
 class Ticket(models.Model):
     title = models.CharField(max_length=128)
-    description = models.TextField(max_length=2048, blank=True)
+    body = models.TextField(max_length=2048, blank=True)
     user = models.ForeignKey(
         to=settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     time_created = models.DateTimeField(default=timezone.now)
