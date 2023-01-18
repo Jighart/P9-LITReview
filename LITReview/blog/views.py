76ce4529c0a5807b3c96671d61c392ad1afc8873
Ludovic Andrianoff <landrianoff2@gmail.com>
2023-01-18 22:12:32 +0100

    Basic views for review list and details

--- LITReview/blog/views.py
+++ LITReview/blog/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Review
 
 # Create your views here.
+def review_list(request):
+    reviews = Review.objects.all()
+    return render(request, 'blog/review/list.html', {'reviews': reviews})
+
+
+def review_detail(request, id):
+    review = get_object_or_404(Review, id=id)
+    return render(request, 'blog/review/detail.html', {'review': review})
