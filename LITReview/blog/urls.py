c0f15faa514d249288b757907b9f0b16dd81418c
Ludovic Andrianoff <landrianoff2@gmail.com>
2023-01-19 16:38:22 +0100

    Implemented bootstrap, navigation, pagination

--- LITReview/blog/urls.py
+++ LITReview/blog/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.review_list, name='review_list'),
-    path('review/<id>/', views.review_detail, name='review_detail')
+    # path('', views.review_list, name='review_list'),
+    path('review/<id>/', views.review_detail, name='review_detail'),
+    path('', views.ReviewListView.as_view(), name='review_list'),
 ]
--- LITReview/blog/views.py
+++ LITReview/blog/views.py
@@ -1,10 +1,30 @@
 from django.shortcuts import render, get_object_or_404
+from django.core.paginator import Paginator, EmptyPage,  PageNotAnInteger
+from django.views import generic
 from .models import Review
 
-# Create your views here.
-def review_list(request):
-    reviews = Review.objects.all()
-    return render(request, 'blog/review/list.html', {'reviews': reviews})
+# def review_list(request):
+#     object_list = Review.objects.all()
+#     paginator = Paginator(object_list, 2)
+#     page = request.GET.get('page')
+#     try:
+#         reviews = paginator.page(page)
+#     except PageNotAnInteger:
+#         reviews = paginator.page(1)
+#     except EmptyPage:
+#         reviews = paginator.page(paginator.num_pages)
+#     context = {
+#         'reviews': reviews,
+#         'page': page,
+#     }
+        
+#     return render(request, 'blog/review/list.html', context)
+
+class ReviewListView(generic.ListView):
+    queryset = Review.objects.all()
+    paginate_by = 2
+    template_name = 'blog/review/list.html'
+    context_object_name = 'reviews'
 
 
 def review_detail(request, id):
