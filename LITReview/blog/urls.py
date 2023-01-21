f7ff8779410f5461f5a236cfe377960a3baa0241
Ludovic Andrianoff <landrianoff2@gmail.com>
2023-01-21 20:10:12 +0100

    CRUD implemented for tickets/reviews

--- LITReview/blog/urls.py
+++ LITReview/blog/urls.py
@@ -2,11 +2,14 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    # path('', views.review_list, name='review_list'),
-    path('review/<id>/', views.review_detail, name='review_detail'),
-    path('ticket/<id>/', views.ticket_detail, name='ticket_detail'),
-    path('ticket/<id>/respond/', views.ticket_respond, name='ticket_respond'),
     path('', views.ReviewListView.as_view(), name='review_list'),
     path('create_review/', views.review_create, name='review_create'),
     path('create_ticket/', views.ticket_create, name='ticket_create'),
+    path('review/<id>/', views.review_detail, name='review_detail'),
+    path('review/<id>/edit/', views.review_edit, name='review_edit'),
+    path('review/<id>/delete/', views.review_delete, name='review_delete'),
+    path('ticket/<id>/', views.ticket_detail, name='ticket_detail'),
+    path('ticket/<id>/respond/', views.ticket_respond, name='ticket_respond'),
+    path('ticket/<id>/edit/', views.ticket_edit, name='ticket_edit'),
+    path('ticket/<id>/delete/', views.ticket_delete, name='ticket_delete'),
 ]
--- LITReview/blog/views.py
+++ LITReview/blog/views.py
@@ -1,27 +1,11 @@
 from django.contrib import messages
 from django.shortcuts import render, get_object_or_404, redirect
-from django.core.paginator import Paginator, EmptyPage,  PageNotAnInteger
+from django.core.exceptions import PermissionDenied
 from django.views import generic
 
-from blog.forms import TicketForm, ReviewForm
+from .forms import TicketForm, ReviewForm
 from .models import Ticket, Review
 
-# def review_list(request):
-#     object_list = Review.objects.all()
-#     paginator = Paginator(object_list, 2)
-#     page = request.GET.get('page')
-#     try:
-#         reviews = paginator.page(page)
-#     except PageNotAnInteger:
-#         reviews = paginator.page(1)
-#     except EmptyPage:
-#         reviews = paginator.page(paginator.num_pages)
-#     context = {
-#         'reviews': reviews,
-#         'page': page,
-#     }
-        
-#     return render(request, 'blog/review/list.html', context)
 
 class ReviewListView(generic.ListView):
     queryset = Review.objects.all().order_by(('time_created')).reverse()
@@ -45,50 +29,142 @@ def review_create(request):
         ticket_form = TicketForm(request.POST, request.FILES or None)
         review_form = ReviewForm(request.POST)
         if ticket_form.is_valid() and review_form.is_valid():
-            new_review = form.save(commit=False)
-            new_review.user = request.user
+            new_ticket = Ticket.objects.create(
+                user = request.user,
+                title = ticket_form.cleaned_data['title'],
+                body = ticket_form.cleaned_data['body']
+            )
+            new_ticket.save()
+
+            new_review = Review.objects.create(
+                ticket = new_ticket,
+                user = request.user,
+                headline = review_form.cleaned_data['headline'],
+                body = review_form.cleaned_data['body']
+            )
             new_review.save()
+
+            messages.success(request, 'Your review has been posted!')
             return redirect('review_list')
     
     else:
-        form = ReviewForm()
-    return render(request, 'blog/review/create_review.html', {'form': form})
+        ticket_form = TicketForm()
+        review_form = ReviewForm()
+
+    context = {
+        'ticket_form': ticket_form,
+        'review_form': review_form,
+        'title': 'Nouvelle critique'
+    }
+    return render(request, 'blog/review/create_review.html', context)
 
 
 def ticket_create(request):
     if request.method == 'POST':
-        form = TicketForm(request.POST or None, request.FILES or None)
-        if form.is_valid():
+        ticket_form = TicketForm(request.POST, request.FILES or None)
+        if ticket_form.is_valid():
             new_ticket = Ticket.objects.create(
                 user = request.user,
-                title = form.cleaned_data['title'],
-                body = form.cleaned_data['body']
+                title = ticket_form.cleaned_data['title'],
+                body = ticket_form.cleaned_data['body']
             )
             new_ticket.save()
-            messages.success(request, 'Created')
 
-            # new_ticket = form.save(commit=False)
-            # new_ticket.user = request.user
-            # new_ticket.save()
+            messages.success(request, 'Created')
             return redirect('review_list')
     
     else:
-        form = TicketForm()
-    return render(request, 'blog/review/create_ticket.html', {'form': form})
+        ticket_form = TicketForm()
+    return render(request, 'blog/review/create_ticket.html', {'ticket_form': ticket_form})
 
 
 def ticket_respond(request, id):
     ticket = get_object_or_404(Ticket, id=id)
 
     if request.method == 'POST':
-        form = ReviewForm(request.POST or None, request.FILES or None)
-        if form.is_valid():
-            new_review = form.save(commit=False)
-            new_review.user = request.user
-            new_review.ticket = ticket
+        review_form = ReviewForm(request.POST, request.FILES or None)
+        if review_form.is_valid():
+            new_review = Review.objects.create(
+                ticket = ticket,
+                user = request.user,
+                headline = review_form.cleaned_data['headline'],
+                body = review_form.cleaned_data['body']
+            )
             new_review.save()
+
+            messages.success(request, 'Created')
             return redirect('review_list')
     
     else:
-        form = ReviewForm()
-    return render(request, 'blog/review/create_review.html', {'form': form})
+        review_form = ReviewForm()
+    return render(request, 'blog/review/create_review.html', {'review_form': review_form})
+
+
+def review_edit(request, id):
+    review = get_object_or_404(Review, id=id)
+
+    if review.user != request.user:
+        raise PermissionDenied()
+
+    if request.method == 'POST':
+        review_form = ReviewForm(request.POST, instance=review)
+        if review_form.is_valid():
+            review_form.save()
+
+            messages.success(request, 'Your review has been updated!')
+            return redirect('review_detail', id)
+
+    else:
+        review_form = ReviewForm(instance=review)
+
+    context = {
+        'review_form': review_form,
+        'ticket': review.ticket,
+        # 'title': 'Update Review'
+    }
+    return render(request, 'blog/review/create_review.html', context)
+
+
+def ticket_edit(request, id):
+    ticket = get_object_or_404(Ticket, id=id)
+
+    if ticket.user != request.user:
+        raise PermissionDenied()
+
+    if request.method == 'POST':
+        ticket_form = TicketForm(request.POST, instance=ticket)
+        if ticket_form.is_valid():
+            ticket_form.save()
+
+            messages.success(request, 'Your ticket has been updated!')
+            return redirect('ticket_detail', id)
+
+    else:
+        ticket_form = TicketForm(instance=ticket)
+    return render(request, 'blog/review/create_ticket.html', {'ticket_form': ticket_form})
+
+
+def ticket_delete(request, id):
+    ticket = get_object_or_404(Ticket, id=id)
+
+    if ticket.user != request.user:
+        raise PermissionDenied()
+
+    if request.method == 'POST':
+        ticket.delete()
+        return redirect('review_list')
+
+    return render(request, 'blog/review/delete.html', {'ticket': ticket})
+
+
+def review_delete(request, id):
+    review = get_object_or_404(Review, id=id)
+
+    if review.user != request.user:
+        raise PermissionDenied()
+
+    if request.method == 'POST':
+        review.delete()
+        return redirect('review_list')
+
+    return render(request, 'blog/review/delete.html', {'review': review})
