96b174e101e1ea6cdfdb124c5f5e4a5e2b2e10f7
Ludovic Andrianoff <landrianoff2@gmail.com>
2023-06-19 21:23:52 +0200

    PEP-8 fixes

--- blog/forms.py
+++ blog/forms.py
@@ -3,21 +3,27 @@ from .models import Ticket, Review
 
 
 class TicketForm(forms.ModelForm):
-    title = forms.CharField(max_length=128, help_text='', required=True, widget=forms.TextInput(attrs={'class':'form-control', 'id':'title', 'type':'text', 'placeholder':'Titre'}))
-    body = forms.CharField(max_length=2048, help_text='', required=True, widget=forms.Textarea(attrs={'class':'form-control', 'id':'description', 'style':'height: 10rem', 'placeholder':'Description'}))
-    picture = forms.ImageField(required=False, widget=forms.FileInput(attrs={'class':'form-control', 'id':'picture', 'type':'file'}))
+    title = forms.CharField(max_length=128, help_text='', required=True, widget=forms.TextInput(
+        attrs={'class': 'form-control', 'id': 'title', 'type': 'text', 'placeholder': 'Titre'}))
+    body = forms.CharField(max_length=2048, help_text='', required=True, widget=forms.Textarea(
+        attrs={'class': 'form-control', 'id': 'description', 'style': 'height: 10rem', 'placeholder': 'Description'}))
+    picture = forms.ImageField(required=False,
+                               widget=forms.FileInput(attrs={'class': 'form-control', 'id': 'picture', 'type': 'file'}))
 
-    
     class Meta:
         model = Ticket
         fields = ['title', 'body', 'picture']
 
 
 class ReviewForm(forms.ModelForm):
-    headline = forms.CharField(max_length=128, help_text='', required=True, widget=forms.TextInput(attrs={'class':'form-control', 'id':'title', 'type':'text', 'placeholder':'Titre'}))
-    body = forms.CharField(max_length=2048, help_text='', required=True, widget=forms.Textarea(attrs={'class':'form-control', 'id':'description', 'style':'height: 10rem', 'placeholder':'Description'}))
-    rating = forms.ChoiceField(label='Note', initial=0, choices=((0, "0"), (1, "1"), (2, "2"), (3, "3"), (4, "4"), (5, "5")), widget=forms.RadioSelect(attrs={'class':'form-check-input', 'id':'rating'}))
-    
+    headline = forms.CharField(max_length=128, help_text='', required=True, widget=forms.TextInput(
+        attrs={'class': 'form-control', 'id': 'title', 'type': 'text', 'placeholder': 'Titre'}))
+    body = forms.CharField(max_length=2048, help_text='', required=True, widget=forms.Textarea(
+        attrs={'class': 'form-control', 'id': 'description', 'style': 'height: 10rem', 'placeholder': 'Description'}))
+    rating = forms.ChoiceField(label='Note', initial=0,
+                               choices=((0, "0"), (1, "1"), (2, "2"), (3, "3"), (4, "4"), (5, "5")),
+                               widget=forms.RadioSelect(attrs={'class': 'form-check-input', 'id': 'rating'}))
+
     class Meta:
         model = Review
         fields = ['headline', 'rating', 'body']
